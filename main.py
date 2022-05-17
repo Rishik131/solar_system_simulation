00ea576a5a471d3149c15316e6e99b91e29741ba
Rishik Kumar <rishik.k2002@gmail.com>
2022-05-17 14:46:24 +0530

    infinit loop created

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import math
 pygame.init()
 w,h = 800,800
 win = pygame.display.set_mode((w,h))
+pygame.display.set_caption('Planet simulation')
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
