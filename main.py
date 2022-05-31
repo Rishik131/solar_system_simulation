4a367bcc89bb694bfe4de1b71bfb828bfd574198
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-01 04:28:31 +0530

    screen updated

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ pygame.init()
 w,h = 800,800
 win = pygame.display.set_mode((w,h))
 pygame.display.set_caption('Planet simulation')
+white = (255,255,255)
 def main():
     run = True
+    clock = pygame.time.Clock()
     while run:
+        clock.tick(60)
+        win.fill(white)
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
     pygame.quit()
+main()
