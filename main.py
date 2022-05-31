783476eea34fb8ed057d177512623c4afb45ed72
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-01 04:31:54 +0530

    planet class made

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ w,h = 800,800
 win = pygame.display.set_mode((w,h))
 pygame.display.set_caption('Planet simulation')
 white = (255,255,255)
+
+class planet:
+    def __init__(self,x,y,radius,color,mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
 def main():
     run = True
     clock = pygame.time.Clock()
