07c8fcbef64cc8b52109a2939ea337b2df042da4
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-01 05:36:17 +0530

    planets created

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ win = pygame.display.set_mode((w,h))
 pygame.display.set_caption('Planet simulation')
 white = (255,255,255)
 yellow = (255,255,0)
+blue = (100,149,237)
+red = (188,39,50)
+dark_grey = (80,78,81)
 
 #creating planets
 class planet:
     au = 149.6e6 * 1000
     g = 6.67428e-11
-    scale = 250/au # 1au == 100px
+    scale = 200/au # 1au == 100px
     timestep = 3600*24
 
     def __init__(self,x,y,radius,color,mass):
@@ -39,7 +42,11 @@ def main():
     #initialising bodies
     sun = planet(0,0,30,yellow,198892*10**30)
     sun.sun = True
-    planets = [sun]
+    mercury = planet(.387*planet.au,0,8,dark_grey,3.3*10**23)
+    venus = planet(.723*planet.au,0,14,white,4.8685*10**24)
+    earth = planet(-1*planet.au,0,16,blue,5.9742*10**24)
+    mars = planet(-1.524*planet.au,0,12,red,6.39*10**23)
+    planets = [sun,mercury,venus,earth,mars]
 
     #running display window
     while run:
