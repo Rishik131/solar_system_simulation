6329a35ae75a3cb92290ecacd3209d0c9e43c9e8
Rishik Kumar <rishik.k2002@gmail.com>
2022-06-01 06:47:48 +0530

    all done

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class planet:
         self.radius = radius
         self.color = color
         self.mass = mass
+        self.orbit = []
         self.sun = False
         self.dist_to_sun = 0
         self.x_vel = 0
@@ -32,6 +33,14 @@ class planet:
     def draw(self,win):
         x = self.x * self.scale + w/2
         y = self.y * self.scale + h/2
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x,y = point
+                x = x*self.scale + w/2
+                y = y*self.scale + h/2
+                updated_points.append(point)
+            pygame.draw.lines(win,self.color,False,updated_points,2)
         pygame.draw.circle(win,self.color,(x,y),self.radius)
 
     def attraction(self, other):
@@ -47,6 +56,19 @@ class planet:
         force_y = math.sin(theta) * force
         return force_x,force_y
 
+    def update_postion(self,planets):
+        total_fx = total_fy = 0
+        for body in planets:
+            if self == body:
+                continue
+            fx,fy = self.attraction(body)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel = total_fx/self.mass * self.timestep
+        self.y_vel = total_fy/self.mass * self.timestep
+        self.orbit.append((self.x,self.y))
+
 #creating procedure
 def main():
     run = True
@@ -56,20 +78,26 @@ def main():
     sun = planet(0,0,30,yellow,198892*10**30)
     sun.sun = True
     mercury = planet(.387*planet.au,0,8,dark_grey,3.3*10**23)
+    mercury.y_vel = -47.4*1000
     venus = planet(.723*planet.au,0,14,white,4.8685*10**24)
+    venus.y_vel = -35.02 * 1000
     earth = planet(-1*planet.au,0,16,blue,5.9742*10**24)
+    earth.y_vel = 29.783 * 1000
     mars = planet(-1.524*planet.au,0,12,red,6.39*10**23)
+    mars.y_vel = 24.077*1000
     planets = [sun,mercury,venus,earth,mars]
 
     #running display window
     while run:
+        # print(0)
         clock.tick(60)
-        # win.fill(white)
+        win.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         for body in planets:
+            body.update_postion(planets)
             body.draw(win)
         pygame.display.update()
     pygame.quit()
